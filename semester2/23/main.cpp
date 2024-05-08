2c35d1796092c30e768f7709e701578c68c56944
we1lman <andrey.velma2005@gmail.com>
2024-05-09 00:04:41 +0300

    added main file

---
+++ semester2/23/main.cpp
@@ -0,0 +1,25 @@
+#include "BinarySearchTree.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    BinarySearchTree tree;
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+    tree.insert(15); // leaf
+    tree.insert(35); // leaf
+    tree.insert(55); // leaf
+    tree.insert(75); // leaf
+
+    int minLeafValue = tree.getMinLeaf();
+    cout << minLeafValue << endl;
+
+    return 0;
+}
